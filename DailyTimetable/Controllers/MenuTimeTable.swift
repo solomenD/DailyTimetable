56dcf553f420405351b96c03c46930a74b2f7617
solomenD <solomondurua@gmail.com>
2023-01-07 04:14:34 +0200

    Added menu timetable stack

--- DailyTimetable/Controllers/MenuTimeTable.swift
+++ DailyTimetable/Controllers/MenuTimeTable.swift
@@ -18,11 +18,40 @@ class MenuTimeTable: BaseController {
         return label
     }()
     
+    private var wellcomeLable: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "DINAlternate-Bold", size: 15)
+        label.textColor = Resourses.Colors.labelBackground
+        label.text = "Wellcome, Solomon"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var addTaskButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 15
+        button.addTarget(self, action: #selector(addTaskButtonTapped), for: .touchUpInside)
+        button.setTitle("Add Task", for: .normal)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.imageView?.tintColor = .white
+        button.titleLabel?.font = UIFont(name: "DINAlternate-Bold", size: 15)
+        button.titleLabel?.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var elementsStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNavigationBar()
         
+    }
+    
+    @objc private func addTaskButtonTapped() {
+        print("addtask")
     }
     
     private func setNavigationBar() {
@@ -45,7 +74,13 @@ extension MenuTimeTable {
     override func setupViews() {
         super.setupViews()
         
-        view.setupView(dayLable)
+        elementsStackView = UIStackView(arrangedSubviews: [dayLable, wellcomeLable],
+                                        axis: .vertical,
+                                        spacing: 10,
+                                        distribution: .fillProportionally)
+        
+        view.setupView(elementsStackView)
+        view.setupView(addTaskButton)
     }
     
     override func constraintViews() {
@@ -53,8 +88,15 @@ extension MenuTimeTable {
         
         NSLayoutConstraint.activate([
         
-            dayLable.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            dayLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+            elementsStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            elementsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            elementsStackView.widthAnchor.constraint(equalToConstant: 200),
+            elementsStackView.heightAnchor.constraint(equalToConstant: 70),
+            
+            addTaskButton.centerYAnchor.constraint(equalTo: elementsStackView.centerYAnchor),
+            addTaskButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            addTaskButton.widthAnchor.constraint(equalToConstant: 120),
+            addTaskButton.heightAnchor.constraint(equalToConstant: 30)
             
         ])
     }
